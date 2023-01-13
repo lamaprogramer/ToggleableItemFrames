e47ef23cd959668b77a8bd087104f32a97347642
BuildTools <unconfigured@null.spigotmc.org>
2023-01-13 10:12:55 -0500

    Initial commit

---
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/ToggleableItemFrames.java
@@ -0,0 +1,16 @@
+package net.iamaprogrammer.toggleableitemframes;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ToggleableItemFrames implements ModInitializer {
+
+	public static final String MOD_ID = "toggleableitemframes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/ToggleableItemFramesClient.java
@@ -0,0 +1,10 @@
+package net.iamaprogrammer.toggleableitemframes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToggleableItemFramesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
