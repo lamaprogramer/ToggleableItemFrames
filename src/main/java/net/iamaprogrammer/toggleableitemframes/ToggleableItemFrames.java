92fd42f076aa41b7f08849ff6c0bb5d71510d61d
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 14:49:30 -0500

    Version 1.0 release

--- src/main/java/net/iamaprogrammer/toggleableitemframes/ToggleableItemFrames.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/ToggleableItemFrames.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ToggleableItemFrames implements ModInitializer {
-	// comment
 	public static final String MOD_ID = "toggleableitemframes";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
