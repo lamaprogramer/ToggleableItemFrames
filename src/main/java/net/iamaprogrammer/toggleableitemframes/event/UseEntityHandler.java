c4827eacf0243b6ac6cc9dc46e5ef56386a91193
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 21:02:25 -0500

    Added spectator check Add sound when making item frame invisible

--- src/main/java/net/iamaprogrammer/toggleableitemframes/event/UseEntityHandler.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/event/UseEntityHandler.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
@@ -17,12 +16,17 @@ public class UseEntityHandler implements UseEntityCallback {
     @Override
     public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
 
-        if (entity instanceof ItemFrameEntity && hitResult != null && !world.isClient() && player.getMainHandStack().isEmpty() && player.isSneaking() && hand.equals(Hand.MAIN_HAND)) {
+        if (entity instanceof ItemFrameEntity && hitResult != null && !world.isClient() && !player.isSpectator() && player.getMainHandStack().isEmpty() && player.isSneaking() && hand.equals(Hand.MAIN_HAND)) {
             // Make item frame invisible and modify nbt
             IModifyItemFrameNbt frame = (IModifyItemFrameNbt)entity;
             frame.setCurrentlyInvisible(!frame.getCurrentlyInvisible());
 
             ((ItemFrameEntity) entity).setRotation(((ItemFrameEntity) entity).getRotation() - 1);
+            if (((ItemFrameEntity) entity).getHeldItemStack() == ItemStack.EMPTY) {
+                entity.playSound(((ItemFrameEntity) entity).getRotateItemSound(), 1.0f, 1.0f);
+            }
+            ((ItemFrameEntity) entity).getRotateItemSound();
+
             entity.setInvisible(!entity.isInvisible());
             //player.sendMessage(Text.literal("visible: " + !entity.isInvisible()));
         }
