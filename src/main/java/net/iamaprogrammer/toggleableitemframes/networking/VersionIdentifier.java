ad6f405bfedb602d54719186ee0ac1b5e62f2314
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 11:05:29 -0500

    changed version identifier for 1.19.4

--- src/main/java/net/iamaprogrammer/toggleableitemframes/networking/VersionIdentifier.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/networking/VersionIdentifier.java
@@ -4,6 +4,6 @@ import net.iamaprogrammer.toggleableitemframes.ToggleableItemFrames;
 import net.minecraft.util.Identifier;
 
 public class VersionIdentifier {
-    public static final String MOD_VERSION = "3.0.0-1.20.x";
+    public static final String MOD_VERSION = "3.0.0-1.19.x";
     public static final Identifier version_id = new Identifier(ToggleableItemFrames.MOD_ID, ToggleableItemFrames.MOD_ID + "_version_identifier");
 }
