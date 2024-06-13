755a7e98754b2e8caf7561dab7d165441272dde2
wolfdv1 <dwhemailpc@gmail.com>
2024-06-13 22:59:59 +0200

    Updated to 1.21

--- src/main/java/net/iamaprogrammer/toggleableitemframes/networking/packets/ModVersionPacket.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/networking/packets/ModVersionPacket.java
@@ -12,7 +12,7 @@ import net.minecraft.util.Uuids;
 import java.util.UUID;
 
 public record ModVersionPacket(String version) implements CustomPayload {
-    public static final CustomPayload.Id<ModVersionPacket> PACKET_ID = new CustomPayload.Id<>(new Identifier(
+    public static final CustomPayload.Id<ModVersionPacket> PACKET_ID = new CustomPayload.Id<>(Identifier.of(
             ToggleableItemFrames.MOD_ID, ToggleableItemFrames.MOD_ID + "_version_identifier"));
     public static final PacketCodec<ByteBuf, ModVersionPacket> PACKET_CODEC = PacketCodecs.STRING.xmap(ModVersionPacket::new, ModVersionPacket::version).cast();
 
