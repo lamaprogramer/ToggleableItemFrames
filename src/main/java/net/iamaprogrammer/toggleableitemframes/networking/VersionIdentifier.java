866401eae6aaa0721f452140069309cd2602e688
willk <willkinsey47@gmail.com>
2024-10-22 13:35:35 -0400

    Changed version identifier.

--- src/main/java/net/iamaprogrammer/toggleableitemframes/networking/VersionIdentifier.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/networking/VersionIdentifier.java
@@ -1,15 +1,5 @@
 package net.iamaprogrammer.toggleableitemframes.networking;
 
-import io.netty.buffer.ByteBuf;
-import net.iamaprogrammer.toggleableitemframes.ToggleableItemFrames;
-import net.iamaprogrammer.toggleableitemframes.networking.packets.ModVersionPacket;
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
-import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Uuids;
-
 public class VersionIdentifier {
-    public static final String MOD_VERSION = "5.1.0-1.21";
+    public static final String MOD_VERSION = "5.1.0-1.21.2";
 }
